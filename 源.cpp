e010efd38d97985ff2f62ee036d3a4636426ab3a
221bandhh <107937395+221bandhh@users.noreply.github.com>
2022-06-22 13:50:50 +0800

    Add files via upload

---
+++ 源.cpp
@@ -0,0 +1,36 @@
+/////* example1_1.c 在屏幕上输出字符串*/
+////#include <stdio.h>
+////int main()
+////{
+////	int s;
+////	s = 2233;
+////	printf("s=%d\n",s);
+//#include <stdio.h>
+//int MAX (int x,int y)
+//{
+//	if (x>y)
+//	return  x;
+//	else
+//	return  y;
+//}
+//int main()
+//{
+//	int x=20;
+//	int y=30;
+//	int max=0;
+//	max = MAX(x, y);
+//	printf("20与30哪个数字大?%d\n", max);
+//	return 0;
+//判断一个数是否为奇数
+#include <stdio.h>//引入头文件
+int main()//主函数
+{
+	int a;
+		scanf("输入想要判断的数字%d\n", a);
+		int b = % a;
+		if (b == 0)
+		printf("%d\n是偶数", a);
+		else (b != 0)
+			printf("%d\n是奇数"，a);
+	return 0;
+}
